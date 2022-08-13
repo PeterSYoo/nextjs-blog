5a83ead8993c4225745d627e8d89a5b9cbb83ebd
Peter Yoo <trustintofu@gmail.com>
2022-08-12 18:12:07 -0700

    wip: adds slug to post page which utilizes getSaticPaths to only prebuild the pages that exists in Sanity by querying Sanity

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,73 @@
+import { GetStaticProps } from 'next';
+import Header from '../../components/Header';
+import { client, urlFor } from '../../sanity';
+import { Post } from '../../typings';
+
+interface Props {
+  post: Post;
+}
+
+const Post = ({ post }: Props) => {
+  console.log(post);
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+};
+
+export default Post;
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+      slug{
+      current
+    }
+    }`;
+
+  const posts = await client.fetch(query);
+
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "post" && slug.current == $slug][0]{
+    _id,
+    _createdAt,
+    title,
+    author-> {
+    name,
+    image
+    },
+    description,
+    mainImage,
+    slug,
+    body
+    }`;
+
+  const post = await client.fetch(query, {
+    slug: params?.slug,
+  });
+
+  if (!post) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
