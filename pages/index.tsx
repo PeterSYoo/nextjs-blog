a12890e5d67ce780534062b8e19631b757be242f
Peter Yoo <trustintofu@gmail.com>
2022-08-12 17:19:24 -0700

    wip: updates tailwind styling on main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,13 +41,13 @@ const Home = ({ posts }: Props) => {
       </div>
 
       {/* Posts */}
-      <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 lg:p-6">
         {posts.map((post) => {
           return (
             <Link key={post._id} href={`/post/${post.slug.current}`}>
-              <div className="group cursor-pointer overflow-hidden rounded-lg border">
+              <div className="border rounded-lg group cursor-pointer overflow-hidden">
                 <img
-                  className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
+                  className="h-60 w-full object-cover group-hover:scale-105 transtion-transform duration-200 ease-in-out"
                   src={urlFor(post.mainImage).url()}
                   alt=""
                 />
@@ -55,7 +55,7 @@ const Home = ({ posts }: Props) => {
                 <div className="flex justify-between bg-white p-5">
                   <div>
                     <p className="text-lg font-bold">{post.title}</p>
-                    <p>
+                    <p className="text-xs">
                       {post.description} by {post.author.name}
                     </p>
                   </div>
