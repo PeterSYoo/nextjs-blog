8ab24fe3995f4e7964605fa12edfdce10aaf11d2
Peter Yoo <trustintofu@gmail.com>
2022-08-14 16:59:25 -0700

    wip: removes banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ const Home = ({ posts }: Props) => {
 
       <Header />
 
-      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+      {/* <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
         <div className="px-10 space-y-5">
           <h1 className="text-6xl max-w-xl font-serif">
             <span className="underline decoration-black decoration-4">
@@ -38,7 +38,7 @@ const Home = ({ posts }: Props) => {
           src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
           alt=""
         />
-      </div>
+      </div> */}
 
       {/* Posts */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 lg:p-6">
